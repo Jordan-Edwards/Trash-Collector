cb8b16d488387df70569c1b5ded0f10a3a24db65
Jordan-Edwards <edwardsjordan441@gmail.com>
2021-07-01 10:43:28 -0500

    Added Employee Class

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20)
+    zipcode = models.CharField(max_length=5)
