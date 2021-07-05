c524bd12d21437bd6c8509579c0d235964506256
wrightzachary <taylorzw96@gmail.com>
2021-07-05 15:21:56 -0500

    Fixed syntax for registration funtion in customers/view.py

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -24,7 +24,6 @@ def registration(request):
         form = NewServiceForm(request.POST)
         if form.is_valid():
             form.save()
-        new_user.save()
-        return HttpResponseRedirect(reverse('customers:index'))
-    else:
-        return render(request, "customers/register.html")
+            return HttpResponseRedirect(reverse('customers:index'))
+    context = {'form': form}
+    return render(request, "customers/registration.html", context)
